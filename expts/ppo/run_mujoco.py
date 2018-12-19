c671b3438a9ed09b4495c7d336d79d5d69d350a5
Gagan Khandate <gagan.khandate@columbia.edu>
2018-12-19 18:07:19 -0500

    saving minor changes

--- expts/ppo/run_mujoco.py
+++ expts/ppo/run_mujoco.py
@@ -2,7 +2,7 @@
 import numpy as np
 import gym
 
-from stable_baselines.common.cmd_util import robotics_arg_parser, make_robotics_env
+from stable_baselines.common.cmd_util import mujoco_arg_parser
 from stable_baselines import bench, logger
 from stable_baselines.common import set_global_seeds
 from stable_baselines.common.vec_env.vec_normalize import VecNormalize
@@ -20,7 +20,8 @@ def train(env_id, num_timesteps, seed):
     :param seed: (int) Used to seed the random generator.
     """
     def make_env():
-        env_out = make_robotics_env(env_id, seed, allow_early_resets=True)
+        env_out = gym.make(env_id)
+        env_out = bench.Monitor(env_out, logger.get_dir(), allow_early_resets=True)
         return env_out
 
     env = DummyVecEnv([make_env])
@@ -28,8 +29,8 @@ def train(env_id, num_timesteps, seed):
 
     set_global_seeds(seed)
     policy = MlpPolicy
-    model = PPO2(policy=policy, env=env, n_steps=256, nminibatches=128, lam=0.95, gamma=0.99, noptepochs=10,
-                 ent_coef = 0.0, learning_rate=3e-4, cliprange=0.2, verbose=1, tensorboard_log= 'tblog')
+    model = PPO2(policy=policy, env=env, n_steps=2048, nminibatches=32, lam=0.95, gamma=0.99, noptepochs=10,
+                 ent_coef=0.0, learning_rate=3e-4, cliprange=0.2, verbose=1)
     model.learn(total_timesteps=num_timesteps)
 
     return model, env
@@ -39,17 +40,19 @@ def main():
     """
     Runs the test
     """
-    args = robotics_arg_parser().parse_args()
-    logger.configure(folder='logsd')
+    args = mujoco_arg_parser().parse_args()
+    logger.configure()
     model, env = train(args.env, num_timesteps=args.num_timesteps, seed=args.seed)
 
-    logger.log("Running trained model")
-    obs = np.zeros((env.num_envs,) + env.observation_space.shape)
-    obs[:] = env.reset()
-    while True:
-        actions = model.step(obs)[0]
-        obs[:] = env.step(actions)[0]
-        env.render()
+    if args.play:
+        logger.log("Running trained model")
+        obs = np.zeros((env.num_envs,) + env.observation_space.shape)
+        obs[:] = env.reset()
+        while True:
+            actions = model.step(obs)[0]
+            obs[:] = env.step(actions)[0]
+            env.render()
+
 
 if __name__ == '__main__':
     main()
